39b577522cac9e5f6074bbf27a5c1812b3556427
Kenly <kbrice@redventures.com>
2018-09-30 07:40:38 -0400

    Section 8 - Array Chunking... Complete!

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
+
+/*
+
+// Working
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last  = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+
+// Not Working
+function chunk(array, size) {
+  const result = [];
+
+  for (let i = 1; i <= size; i++) {
+    if (array.length > 0) {
+      result.push(array.splice(0,size));
+    }
+  }
+
+  return result;
+}
+
+*/

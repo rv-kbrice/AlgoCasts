ae218144376da393d85b11287d5e3f0434986d50
Kenly <kbrice@redventures.com>
2018-09-30 17:41:12 -0400

    Section 16 - Runtime Complexity in Practice - Fibonacci... Complete!

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,51 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) return cache[args];
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function slowFib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+
+/*
+
+
+// Exponential time solution
+// will take too long to execute.
+function slowFib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
+
+*/

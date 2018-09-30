394c165c9061c196475281bcc6fa48e7dce55663
Kenly <kbrice@redventures.com>
2018-09-30 06:00:25 -0400

    Section 5 - Ingeter Reversal... Complete!

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,32 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Third Solution
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+/*
+NOTE:
+parseInt() convert string into number
+.toString() to convert numbers into strings
+Math.sign() returns whether number is negative (-1) or positive (1)
+
+
+// Second Solution
+function reverseInt(n) {
+  let str = Math.sign(n) < 0 ? -n : n;
+  let reverse = parseInt(str.toString().split('').reverse().join(''));
+  return Math.sign(n) < 0 ? -reverse : reverse;
+}
+
+// First Solution
+function reverseInt(n) {
+  let str = String(n < 0 ? -n : n);
+  let reverse = Number(str.split('').reverse().join(''));
+  return (n < 0) ?  -reverse : reverse;
+}
+*/

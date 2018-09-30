eeb8bd35357221283c1b8696d677f8fcd06e1c25
Kenly <kbrice@redventures.com>
2018-09-30 15:12:36 -0400

    Section 10 - Sequence Capitalization... Complete!

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,45 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length; i++) {
+    (str[i - 1] === ' ') ? result += str[i].toUpperCase() : result += str[i];
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
+
+
+/*
+
+// Working version
+function capitalize(str) {
+  let words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
+}
+
+
+// My Attempt
+function capitalize(str) {
+  const stringArr = str.split(' ');
+  let wordsArr = [];
+
+  for (let word of stringArr) {
+    let arr = word.split('');
+    arr = [...arr[0].toUpperCase(), ...arr.slice(1)];
+    wordsArr.push(arr.join(''));
+  }
+
+  return wordsArr.join(' ');
+}
+
+*/
